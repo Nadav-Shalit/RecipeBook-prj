6aa12584528136e3ae209ee2e71b02516979768e
nadav <nadavs@azrielidigital.com>
2021-12-08 12:13:12 +0200

    missing file of store ngex

--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -1,18 +1,48 @@
-// import { Action } from '@ngrx/store';
+import { Action } from "@ngrx/store";
 
-import { Ingredient } from 'src/app/shared/ingredient.model';
-
+import { Ingredient } from "src/app/shared/ingredient.model";
 
 export enum INGREDIENT_ACTIONS {
-    ADD_INGREDIENT = 'ADD_INGREDIENT',
-    UPDATE_INGREDIENT = 'UPDATE_INGREDIENT',
-    DELETE_INGREDIENT = 'DELETE_INGREDIENT',
-    CLEAR_INGREDIENT = 'CLEAR_INGREDIENT'    
-  }
+  ADD_INGREDIENT = "ADD_INGREDIENT",
+  ADD_MULTI_INGREDIENTS = "ADD_MULTI_INGREDIENTS",
+  UPDATE_INGREDIENT = "UPDATE_INGREDIENT",
+  DELETE_INGREDIENT = "DELETE_INGREDIENT",
+  CLEAR_INGREDIENTS = "CLEAR_INGREDIENTS",
+  START_EDIT = "START_EDIT",
+  STOP_EDIT = "STOP_EDIT",
+}
 
-//  export class AddIngredient implements Action{
-//      readonly type = INGREDIENT_ACTIONS.ADD_INGREDIENT;
-//      payload:Ingredient;
-
-
-//     } 
+export type Actions =
+  | AddIngredient
+  | AddMultiIngredients
+  | UpdateIngredient
+  | DeleteIngredient
+  | ClearIngredients
+  | StartEdit
+  | StopEdit;
+export class AddIngredient implements Action {
+  readonly type = INGREDIENT_ACTIONS.ADD_INGREDIENT;
+  constructor(public payload: Ingredient) {}
+}
+export class AddMultiIngredients implements Action {
+  readonly type = INGREDIENT_ACTIONS.ADD_MULTI_INGREDIENTS;
+  constructor(public payload: Ingredient[]) {}
+}
+export class UpdateIngredient implements Action {
+  readonly type = INGREDIENT_ACTIONS.UPDATE_INGREDIENT;
+  constructor(public payload: { ingredient: Ingredient }) {}
+}
+export class DeleteIngredient implements Action {
+  readonly type = INGREDIENT_ACTIONS.DELETE_INGREDIENT;
+  constructor() {}
+}
+export class ClearIngredients implements Action {
+  readonly type = INGREDIENT_ACTIONS.CLEAR_INGREDIENTS;
+}
+export class StartEdit implements Action {
+  readonly type = INGREDIENT_ACTIONS.START_EDIT;
+  constructor(public payload: number) {}
+}
+export class StopEdit implements Action {
+  readonly type = INGREDIENT_ACTIONS.STOP_EDIT;
+}
