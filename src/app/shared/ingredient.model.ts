95efca139a2bfb26a7a3c075e05240ce3ec1f207
nadav <nadavs@azrielidigital.com>
2021-12-12 17:36:27 +0200

    add upsertIngredient logic and recpice name

--- src/app/shared/ingredient.model.ts
+++ src/app/shared/ingredient.model.ts
@@ -1,19 +1,22 @@
-
 export class Ingredient {
-  recipeNames:string[] = [];
- 
-  constructor(public name: string, public amount: number, private recipeName:string= "") {
-    if(recipeName && this.recipeNames.indexOf(recipeName)===-1){
+  recipeNames: string[] = [];
+
+  constructor(
+    public name: string,
+    public amount: number,
+    private recipeName: string = ""
+  ) {
+    if (recipeName && this.recipeNames.indexOf(recipeName) === -1) {
       this.recipeNames.push(recipeName);
     }
   }
- 
-   addRecipeNames(recipeName:string) {
-    if(recipeName && this.recipeNames.indexOf(recipeName)===-1){
+
+  addRecipeNames(recipeName: string) {
+    if (recipeName && this.recipeNames.indexOf(recipeName) === -1) {
       this.recipeNames.push(recipeName);
     }
   }
-  recipesList():string{
-    return this.recipeNames.join(',').trim(); 
+  recipesList(): string {
+    return this.recipeNames ? this.recipeNames.join(",").trim() : "";
   }
 }
--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -1,3 +1,4 @@
+import { Recipe } from "./../../recipes/recipe.model";
 import { Ingredient } from "src/app/shared/ingredient.model";
 import * as ShoppingListActios from "./shopping-list.actions";
 
@@ -23,22 +24,35 @@ export function shoppingListReducer(
   state: State = initState,
   action: ShoppingListActios.Actions
 ) {
+  let updateIngredients: Ingredient[] = [...state.ingredients];
   switch (action.type) {
     case ShoppingListActios.INGREDIENT_ACTIONS.ADD_INGREDIENT:
+      let ingredient: Ingredient = action.payload;
+      updateIngredients = upsertIngredient(updateIngredients, [ingredient]);
+
       return {
-        ...state, // No need since we have only one propety, but is a good practice to copy the old state
-        ingredients: [...state.ingredients, action.payload],
+        ...state,
+        ingredients: updateIngredients,
+        selectedIndex: -1,
+        curIngredient: null,
       };
       break;
     case ShoppingListActios.INGREDIENT_ACTIONS.ADD_MULTI_INGREDIENTS:
+      updateIngredients = upsertIngredient(updateIngredients, [
+        ...action.payload,
+      ]);
       return {
         ...state, // No need since we have only one propety, but is a good practice to copy the old state
-        ingredients: [...state.ingredients, ...action.payload],
+        ingredients: updateIngredients,
+        selectedIndex: -1,
+        curIngredient: null,
       };
       break;
     case ShoppingListActios.INGREDIENT_ACTIONS.UPDATE_INGREDIENT:
-      const updateIngredients: Ingredient[] = [...state.ingredients];
       updateIngredients[state.selectedIndex] = action.payload.ingredient;
+      // updateIngredients[state.selectedIndex].recipeNames = [
+      //   ...action.payload.ingredient.recipeNames,
+      // ];
       return {
         ...state,
         ingredients: updateIngredients,
@@ -87,4 +101,31 @@ export function shoppingListReducer(
       return state;
       break;
   }
+}
+function upsertIngredient(
+  updateIngredients: Ingredient[],
+  newIngredients: Ingredient[]
+) {
+  newIngredients.forEach((newIng) => {
+    const idx = updateIngredients.findIndex((ing) => {
+      return ing.name.toLocaleLowerCase() === newIng.name.toLocaleLowerCase();
+    });
+    if (idx === -1) {
+      updateIngredients = [...updateIngredients, newIng];
+    } else {
+      const recipeNames: string[] = [...updateIngredients[idx].recipeNames];
+      updateIngredients[idx] = new Ingredient(
+        newIng.name,
+        updateIngredients[idx].amount + newIng.amount
+      );
+      newIng.recipeNames.forEach((rcp) => {
+        recipeNames.push(rcp);
+      });
+      recipeNames.forEach((rcp) => {
+        updateIngredients[idx].addRecipeNames(rcp);
+      });
+    }
+  });
+
+  return updateIngredients;
 }
